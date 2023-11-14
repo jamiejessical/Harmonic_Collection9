25fe888881f39889831d835bea4f9e8599954cbd
jamiejessical <144182973+jamiejessical@users.noreply.github.com>
2023-11-14 01:15:40 -0500

    Add files via upload

---
+++ Harmonic Collection/js/Script9.js
@@ -0,0 +1,16 @@
+
+function opacityChange() {
+    var rawprawn1Div = document.getElementById ("rawprawn1");
+    rawprawn1Div.style.animation = "animation 10s"
+}
+
+function opacityChange2() {
+    var rawprawn2Div = document.getElementById ("rawprawn2");
+    rawprawn2Div.style.animation = "animation 10s"
+}
+
+function opacityChange3() {
+    var rawprawn3Div = document.getElementById ("rawprawn3");
+    rawprawn3Div.style.animation = "animation 10s"
+}
+
