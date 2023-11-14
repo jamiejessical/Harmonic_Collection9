8bdc6bbd8755649e51ae287f7f46924d94414ab2
jamiejessical <144182973+jamiejessical@users.noreply.github.com>
2023-11-14 01:53:31 -0500

    Update Script9.js

--- Harmonic Collection/js/Script9.js
+++ Harmonic Collection/js/Script9.js
@@ -1,16 +1,19 @@
 
 function opacityChange() {
     var rawprawn1Div = document.getElementById ("rawprawn1");
-    rawprawn1Div.style.animation = "animation 10s"
+    rawprawn1Div.style.animation = "animation 10s";
+    rawprawn1Div.style.animationFillMode = "forwards";
 }
 
 function opacityChange2() {
     var rawprawn2Div = document.getElementById ("rawprawn2");
-    rawprawn2Div.style.animation = "animation 10s"
+    rawprawn2Div.style.animation = "animation 10s";
+    rawprawn2Div.style.animationFillMode = "forwards";
 }
 
 function opacityChange3() {
     var rawprawn3Div = document.getElementById ("rawprawn3");
-    rawprawn3Div.style.animation = "animation 10s"
+    rawprawn3Div.style.animation = "animation 10s";
+    rawprawn3Div.style.animationFillMode = "forwards";
 }
 
